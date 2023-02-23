9ca3b2510c101be68906ac5e8afaf6e7dc83625c
Dhruv Harsora <harsoradhruv@gmail.com>
2023-02-23 11:47:44 +0530

    feat: initial library setup

---
+++ src/theme/index.ts
@@ -0,0 +1,4 @@
+import { horizontalScale, moderateScale, verticalScale } from './Metrics';
+
+export { default as Colors } from './Colors';
+export { horizontalScale, moderateScale, verticalScale };
